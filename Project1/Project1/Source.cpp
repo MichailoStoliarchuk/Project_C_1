23df8000707251f3d3a8dedb240795bd94ef8086
Mickhailo Stoliarchuk <miha0206liza@gmail.com>
2024-07-08 15:30:14 +0300

    Create Dz one

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,10 +1,47 @@
+//Завдання 1
 
-
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-int main()
-{
-		
+int main() {
+    int n = 0;
+    int z = 0;
+    int b = 0;
+    char str[100];
+    char word[100];
+
+    cout << "Enter sentence " << endl;
+    cin.getline(str, 100);
+
+    cout << "Enter word " << endl;
+    cin >> word;
+
+    for (int j = 0; word[j] != '\0'; j++) {
+        z++;
+    }
+
+    for (int i = 0; str[i] != '\0'; i++) {
+
+        if (str[i] == word[0]) {
+
+            int k = 0;
+
+            while (word[k] != '\0' && str[i + k] == word[k]) {
+                k++;
+            }
+            if (word[k] == '\0') {
+
+                if (str[i + k] == ' ') {
+                    b++;
+                }
+                else if (str[i + k] == '\0') {
+                    b++;
+                }
+            }
+        }
+    }
+
+    cout << "Words " << b << endl;
+
+    return 0;
 }
